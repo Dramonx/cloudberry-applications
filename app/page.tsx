8d1a96343e8cef3ec3395d8cd1749ea0db5feeb9
Dramonx <dramonxofficial@gmail.com>
2026-05-01 20:39:39 -0700

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+export default function Page() {
+  return (
+    <div style={{
+      display: "flex",
+      height: "100vh",
+      alignItems: "center",
+      justifyContent: "center",
+      fontSize: "2rem",
+      fontWeight: "bold"
+    }}>
+      CloudBerry Applications Loading...
+    </div>
+  );
+}
