138c4a472973c781d8fffbf5f7d15774e6cbb79d
Dramonx <dramonxofficial@gmail.com>
2026-05-06 06:47:44 +0000

    Update route.ts

--- app/api/status/route.ts
+++ app/api/status/route.ts
@@ -1,25 +1,26 @@
 export async function GET() {
   try {
-    // Minecraft status
-    const mcRes = await fetch("https://api.mcstatus.io/v2/status/java/mc-cloudberry.net");
-    const mc = await mcRes.json();
+    const mc = await fetch(
+      "https://api.mcstatus.io/v2/status/java/mc.cloudberry.net"
+    ).then(res => res.json());
 
-    // Discord total member count
-    const discordRes = await fetch("https://discord.com/api/guilds/1263235716750512199/preview");
+    const discordRes = await fetch(
+      "https://discord.com/api/guilds/1263235716750512199/widget.json"
+    );
 
     const discord = await discordRes.json();
 
     return Response.json({
       players: mc.players?.online ?? 0,
       max: mc.players?.max ?? 0,
-      discord: discord.approximate_member_count ?? "N/A",
+      discord: discord.presence_count ?? "0",
     });
 
   } catch (err) {
     return Response.json({
       players: 0,
       max: 0,
-      discord: "0"
+      discord: 0,
     });
   }
 }
