7f46b79809e8159a2be8881af9f2dd12dc654be7
Dramonx <dramonxofficial@gmail.com>
2026-05-07 17:06:25 -0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,13 +7,13 @@ export const metadata = {
 };
 
 export default function RootLayout({
-  children
+  children,
 }: {
   children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body style={{ margin: 0 }}>
+      <body>
         <Header />
         {children}
       </body>
