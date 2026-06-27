54653aeb369766036c44a7cdcaab046f2f8413b4
Dramonx <dramonxofficial@gmail.com>
2026-06-26 22:32:52 -0700

    Prefer crawlable favicon metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,11 +3,15 @@ import "./home-spacing.css";
 import Header from "./components/Header";
 
 export const metadata = {
+  metadataBase: new URL("https://www.mc-cloudberry.com"),
   title: "Welcome to CloudBerry",
   description: "Gen-PVP, Survival, Events!",
   icons: {
-    icon: "/CloudBerry.png",
-    shortcut: "/CloudBerry.png",
+    icon: [
+      { url: "/favicon.svg", type: "image/svg+xml" },
+      { url: "/CloudBerry.png", sizes: "1254x1254", type: "image/png" },
+    ],
+    shortcut: "/favicon.svg",
     apple: "/CloudBerry.png",
   },
 };
